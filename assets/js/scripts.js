e3fe07960b5e95efae7059feaca01fd5a88fe6a1
EllenSt264 <stafford.ellen@protonmail.com>
2020-09-07 15:49:46 +0100

    Fix: navbar fullscreen overlay menu
    Added javascript to close the menu automatically when a navbar link is clicked.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,9 +3,30 @@
 //        "https://www.codeply.com/go/62Roy6RDOa"
 
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 100) {
-      $('.navbar, .navbar-brand').addClass('affix');
-    } else {
-      $('.navbar, .navbar-brand').removeClass('affix');
-    }
-  });
+  if ($(document).scrollTop() > 100) {
+    $('.navbar, .navbar-brand').addClass('affix');
+  } else {
+    $('.navbar, .navbar-brand').removeClass('affix');
+  }
+});
+
+
+//        The following code was
+//        based upon:
+//        "https://teamtreehouse.com/community/how-to-close-toggle-overlay-menu-when-clicking-navigation-item-anchor-link"
+
+//  Closes the navbar fullscreen overlay menu when 
+//  a navlink is clicked.
+
+$('.navbar-toggler').click(function() {
+  $(this).toggleClass('active');
+  $('.navbar-collapse').toggleClass('open').show();
+ });
+
+
+// Closes the menu after clicking link
+
+$('.navbar-collapse li').on('click', function(){
+   $(".navbar-collapse").hide();
+   $(".navbar-toggler").removeClass("active");
+});
